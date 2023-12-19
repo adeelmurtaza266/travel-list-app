9eea94673c59cf77c98fcfc2eeb9cbf0516ff56e
adeelmurtaza266 <adeelmurtaza266@gmail.com>
2023-12-19 13:33:37 +0500

    trevel item added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
+import { useState } from 'react';
 import './index.css';
 import Header from './components/Header.js';
 import Stats from './components/Stats.js';
 import PackingList from './components/PackingList.js';
 import Form from './components/Form.js';
 
-const initialItems = [
-  {id: 1, description:'', quantity: 1, packed: false,},
-  {id: 2, description:'', quantity: 2, packed: true,},
-]
+
 function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Header />
-      <Form />
-      <PackingList initialItems={initialItems} />
+      <Form onAddItem={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,15 +1,10 @@
 import '../index.css';
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Charger", quantity: 3, packed: false },
-];
 function PackingList({ items}) {
   return( 
     <div className="list">
       <ul>
-        {initialItems.map((item)=> (<Item item={item} key={item.id} />))}
+        {items.map((item)=> (<Item item={item} key={item.id} />))}
       </ul>
     </div>
   );
